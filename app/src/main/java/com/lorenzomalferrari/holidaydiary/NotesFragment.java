dc2ae153505139214eee6f071f34750cef6dfd4a
Lorenzo Malferrari <malfe.lore@gmail.com>
2018-04-12 10:16:24 +0200

    Upload Holiday Diary App

---
+++ app/src/main/java/com/lorenzomalferrari/holidaydiary/NotesFragment.java
@@ -0,0 +1,37 @@
+package com.lorenzomalferrari.holidaydiary;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NotesFragment extends Fragment {
+
+
+    public NotesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_notes, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle(R.string.notes);
+    }
+
+}
