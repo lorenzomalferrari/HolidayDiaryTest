c81d51848d0a9f6024ee7366a407162d9ecbfe7e
Lorenzo Malferrari <malfe.lore@gmail.com>
2018-04-12 16:45:51 +0200

    Create User.java -- Base

---
+++ app/src/main/java/com/lorenzomalferrari/holidaydiary/model/User.java
@@ -0,0 +1,13 @@
+package com.lorenzomalferrari.holidaydiary.model;
+
+import java.util.Date;
+
+public class User {
+
+    private String username, firstName, lastName, email, password, gender, city, country;
+    private int age;
+    private Date birthdate;
+
+
+
+}
