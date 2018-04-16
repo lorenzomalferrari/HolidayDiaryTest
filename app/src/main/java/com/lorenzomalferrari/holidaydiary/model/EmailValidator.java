b84a133e6209aa5e355ac0a2e6d6dac582afe973
Lorenzo Malferrari <malfe.lore@gmail.com>
2018-04-16 11:10:16 +0200

    Add class EmailValidator

---
+++ app/src/main/java/com/lorenzomalferrari/holidaydiary/model/EmailValidator.java
@@ -0,0 +1,53 @@
+package com.lorenzomalferrari.holidaydiary.model;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Java email validation program
+ * @author lorenzomalferrari
+ */
+
+public class EmailValidator {
+    // Email Regex java
+    private static final String EMAIL_REGEX = "^[\\w-\\+]+(\\.[\\w]+)*@[\\w-]+(\\.[\\w]+)*(\\.[a-z]{2,})$";
+
+    // static Pattern object, since pattern is fixed
+    private static Pattern pattern;
+
+    // non-static Matcher object because it's created from the input String
+    private Matcher matcher;
+
+    public EmailValidator() {
+        // initialize the Pattern object
+        pattern = Pattern.compile(EMAIL_REGEX, Pattern.CASE_INSENSITIVE);
+    }
+
+    /**
+     * This method validates the input email address with EMAIL_REGEX pattern
+     *
+     * @param email
+     * @return boolean
+     */
+    public boolean validateEmail(String email) {
+        matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    /**
+     * Test
+     * malfe.lore@gmail.com --> true
+     * malfe.loregmail.com --> false
+     * 
+     * Esito corretto
+     */
+    /*public static void main(String args[]) {
+        System.out.println("-----------------------------");
+        EmailValidator emailV = new EmailValidator();
+        boolean v = emailV.validateEmail("malfe.lore@gmail.com");
+        boolean v2 = emailV.validateEmail("malfe.loregmail.com");
+        System.out.println(v);
+        System.out.println(v2);
+        System.out.println("-----------------------------");
+    }*/
+}
