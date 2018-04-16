16cb2eb502188b1c6e92ea1c205554fafd3b7117
Lorenzo Malferrari <malfe.lore@gmail.com>
2018-04-16 16:23:24 +0200

    Updates

--- app/src/main/java/com/lorenzomalferrari/holidaydiary/model/PasswordValidator.java
+++ app/src/main/java/com/lorenzomalferrari/holidaydiary/model/PasswordValidator.java
@@ -11,10 +11,27 @@ public class PasswordValidator {
      *
      *  - 2 numeri
      *  - 1 maiuscola
+     *  - 1 minuscola
      *
      * Da quanti caratteri dovrà essere composta la password?
      *
      *  - 6 caratteri
      */
 
+    /**
+     * Metodo che mi dice se la lunghezza della password è accettabile
+     * @param password
+     * @return >= 6 Accettabile (true)
+     *
+     */
+    private boolean isLengthCorrect(String password){
+        boolean status = false;
+        if (password.length() >= 6){
+            return status = true;
+        }
+        else
+        {
+            return status;
+        }
+    }
 }
