f46cc8347055059ffdccd4522cab6506c4a18bb7
Lorenzo Malferrari <malfe.lore@gmail.com>
2018-04-24 16:10:35 +0200

    Deleted code

--- app/src/main/java/com/lorenzomalferrari/holidaydiary/LoginActivity.java
+++ app/src/main/java/com/lorenzomalferrari/holidaydiary/LoginActivity.java
@@ -45,8 +45,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //interrogo il db per sapere se email e password sono già presenti e se hanno lo stesso id
-                Cursor c = databaseHelper.getDataLogin(emailText.getText().toString(),passwordText.getText().toString());
-                Toast.makeText(LoginActivity.this,c.getString(0),Toast.LENGTH_LONG).show();
+
                 //if profilo esiste già
                 if (emailText.getText().toString().equals(user.getEmail()) && passwordText.getText().toString().equals(user.getPassword())){
                     callMenu();
