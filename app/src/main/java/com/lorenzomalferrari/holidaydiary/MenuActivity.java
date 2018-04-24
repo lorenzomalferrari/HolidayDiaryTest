8e69bf712df3c7aee49f6cbafd060f67f552e45a
Lorenzo Malferrari <malfe.lore@gmail.com>
2018-04-24 12:23:35 +0200

    Delete import that app doesn't use

--- app/src/main/java/com/lorenzomalferrari/holidaydiary/MenuActivity.java
+++ app/src/main/java/com/lorenzomalferrari/holidaydiary/MenuActivity.java
@@ -1,11 +1,8 @@
 package com.lorenzomalferrari.holidaydiary;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -14,8 +11,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import com.lorenzomalferrari.holidaydiary.model.DatabaseHelper;
 
 public class MenuActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
