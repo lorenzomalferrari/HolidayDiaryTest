65fbb3d116a4a9a119c282825654816462ca0e67
Lorenzo Malferrari <malfe.lore@gmail.com>
2018-04-26 10:28:52 +0200

    Updates

--- app/src/main/java/com/lorenzomalferrari/holidaydiary/HomePageFragment.java
+++ app/src/main/java/com/lorenzomalferrari/holidaydiary/HomePageFragment.java
@@ -1,12 +1,17 @@
 package com.lorenzomalferrari.holidaydiary;
 
 
+import android.app.AlertDialog;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.lorenzomalferrari.holidaydiary.model.DatabaseHelper;
 
 
 /**
@@ -14,6 +19,8 @@ import android.view.ViewGroup;
  */
 public class HomePageFragment extends Fragment {
 
+    DatabaseHelper databaseHelper;
+    Button btnVisaulizzaDati;
 
     public HomePageFragment() {
         // Required empty public constructor
@@ -32,6 +39,45 @@ public class HomePageFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         //you can set the title for your toolbar here for different fragments different titles
         getActivity().setTitle(R.string.homepage);
+    }
+
+
+    public void viewTableData(){
+        btnVisaulizzaDati.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Cursor cursor = databaseHelper.getAllData();
+                        if (cursor.getCount() == 0){
+                            //show message
+                            showMessage("Errore","Trovo niente");
+                            return;
+                        }
+                        else {
+                            StringBuffer buffer = new StringBuffer();
+                            while (cursor.moveToNext()){
+                                buffer.append("Id : "+ cursor.getString(0)+"\n");
+                                buffer.append("Nome : "+ cursor.getString(1)+"\n");
+                                buffer.append("Cognome : "+ cursor.getString(2)+"\n");
+                                buffer.append("Username : "+ cursor.getString(3)+"\n");
+                                buffer.append("Password : "+ cursor.getString(4)+"\n");
+                                buffer.append("City : "+ cursor.getString(5)+"\n");
+                            }
+
+                            // show all data
+                            showMessage("Data",buffer.toString());
+                        }
+                    }
+                }
+        );
+    }
+
+    public void showMessage(String title, String Message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
     }
 
 }
