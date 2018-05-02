cae3ac09f4c47da326280b458b298496a392f218
Lorenzo Malferrari <malfe.lore@gmail.com>
2018-05-02 10:33:33 +0200

    Add class

---
+++ app/src/main/java/com/lorenzomalferrari/holidaydiary/model/Validator.java
@@ -0,0 +1,19 @@
+package com.lorenzomalferrari.holidaydiary.model;
+
+public class Validator {
+
+
+
+
+
+    /**
+     * Calcolo l'età dell'utente usando data odierna e data di nascita inserita
+     * (Metodo ancora da costruire e da mettere in altra classe)
+     * @return
+     */
+    public int calcAge(){
+        int age = 0;
+
+        return age;
+    }
+}
