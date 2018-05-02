1642f26cfa344534e341f5161dce1d24ffff6b29
Lorenzo Malferrari <malfe.lore@gmail.com>
2018-05-02 10:58:13 +0200

    Cleaning

--- app/src/main/java/com/lorenzomalferrari/holidaydiary/NotesFragment.java
+++ app/src/main/java/com/lorenzomalferrari/holidaydiary/NotesFragment.java
@@ -1,13 +1,11 @@
 package com.lorenzomalferrari.holidaydiary;
 
-
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,7 +16,6 @@ public class NotesFragment extends Fragment {
     public NotesFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
