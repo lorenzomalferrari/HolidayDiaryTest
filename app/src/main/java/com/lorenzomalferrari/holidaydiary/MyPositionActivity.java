8e85618a3063f9c930f0dfd50e75afb895d117ec
Lorenzo Malferrari <malfe.lore@gmail.com>
2018-05-03 15:35:34 +0200

    Updates

--- app/src/main/java/com/lorenzomalferrari/holidaydiary/MyPositionActivity.java
+++ app/src/main/java/com/lorenzomalferrari/holidaydiary/MyPositionActivity.java
@@ -1,32 +1,16 @@
 package com.lorenzomalferrari.holidaydiary;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.location.Location;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.widget.Toast;
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MyPositionActivity extends FragmentActivity implements OnMapReadyCallback,
-                GoogleApiClient.ConnectionCallbacks,
-                GoogleApiClient.OnConnectionFailedListener,
-                LocationListener{
+public class MyPositionActivity extends FragmentActivity implements OnMapReadyCallback{
 
     private GoogleMap mMap;
 
@@ -54,122 +38,7 @@ public class MyPositionActivity extends FragmentActivity implements OnMapReadyCa
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-        //set location
-        setUpLocation();
     }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-
-        switch (requestCode){
-            case MY_PERMISSION_REQUEST_CODE:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    if (checkPlayServices()){
-                        buildGoogleApiClient();
-                        createLocationRequest();
-                        displayLocation();
-                    }
-                }
-                break;
-        }
-    }
-
-    private void setUpLocation() {
-        if (android.support.v4.app.ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && android.support.v4.app.ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-            requestRuntimePermission();
-        }
-        else {
-            if (checkPlayServices()){
-                buildGoogleApiClient();
-                createLocationRequest();
-                displayLocation();
-                Toast.makeText(this,"Longitudine: "+longitude+"  Latitudine: "+latitude, Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    private void displayLocation() {
-        if (android.support.v4.app.ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && android.support.v4.app.ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
-        {
-            return;
-        }
-        myLastLocation = LocationServices.FusedLocationApi.getLastLocation(myGoogleApiClient);
-        if (myLastLocation != null)
-        {
-            latitude = myLastLocation.getLatitude();
-            longitude = myLastLocation.getLongitude();
-
-
-            //add Marker
-            if (myCurrent != null)
-            {
-                myCurrent.remove(); // remove old marker
-            }
-            myCurrent = mMap.addMarker(new MarkerOptions()
-                                .position(new LatLng(latitude,longitude))
-                                .title("YUO"));
-
-            //move camera to this position
-            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude,longitude),12.0f));
-        }
-    }
-
-    private void createLocationRequest() {
-        myLocationRequest = new LocationRequest();
-        myLocationRequest.setInterval(UPDATE_INTERVAL);
-        myLocationRequest.setFastestInterval(FASTEST_INTERVAL);
-        myLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        myLocationRequest.setSmallestDisplacement(DISPLACEMENT);
-    }
-
-    protected synchronized void buildGoogleApiClient() {
-        myGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API).build();
-        myGoogleApiClient.connect();
-    }
-
-    private boolean checkPlayServices() {
-        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
-        if (resultCode != ConnectionResult.SUCCESS)
-        {
-            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode))
-            {
-                GooglePlayServicesUtil.getErrorDialog(resultCode,this,PLAY_SERVICES_RESOLUTION_REQUEST).show();
-            }
-            else
-                {
-                Toast.makeText(this,"This device can't support", Toast.LENGTH_SHORT).show();
-                finish();
-            }
-            return false;
-        }
-        return true;
-    }
-
-
-    private void requestRuntimePermission() {
-        android.support.v4.app.ActivityCompat.requestPermissions(this,new String[]
-                {
-                        Manifest.permission.ACCESS_COARSE_LOCATION,
-                        Manifest.permission.ACCESS_FINE_LOCATION,
-                },MY_PERMISSION_REQUEST_CODE);
-    }
-
-    private void startLocationUpdates()
-    {
-        if (android.support.v4.app.ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && android.support.v4.app.ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
-        {
-            return;
-        }
-        LocationServices.FusedLocationApi.requestLocationUpdates(myGoogleApiClient,myLocationRequest,this);
-    }
-
 
     /**
      * Manipulates the map once available.
@@ -190,25 +59,4 @@ public class MyPositionActivity extends FragmentActivity implements OnMapReadyCa
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        displayLocation();
-        startLocationUpdates();
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        myGoogleApiClient.connect();
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        myLastLocation = location;
-        displayLocation();
-    }
 }
